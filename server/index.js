b275c3fc9f705b8aae62e7468f704819afee0b37
LohithaMaddula <lohitha.m003@gmail.com>
2024-09-24 22:07:53 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,25 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import connectDB from './configs/db.js'
-import notesRoute from './routes/notesRoute.js'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import notesRoute from "./routes/notesRoute.js";
 
-const app = express()
-const PORT = process.env.PORT || 8080
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
 
-connectDB()
+connectDB();
 
-app.use(express.json())
-app.use(cors())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('Keep clone backend')
-})
+app.get("/", (req, res) => {
+  res.send("Keep clone backend");
+});
 
-app.use('/api', notesRoute)
+app.use("/api", notesRoute);
 
 app.listen(PORT, () => {
-  console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
